a731a6ff5654631ceca3204fd54dc10baf88106a
Ilyas-Ait-Ali <ilyasgameraa@gmail.com>
2025-07-22 22:43:11 -0400

    Updates

---
+++ precache-manifest.3307a200e6d483de6de21b3ed49b52b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0382ea83f9e25b6cadbb61491f889157",
+    "url": "/SuppliesPro-Website/index.html"
+  },
+  {
+    "revision": "3f42c4fbfb720eda8be5",
+    "url": "/SuppliesPro-Website/static/css/main.f3481504.chunk.css"
+  },
+  {
+    "revision": "0306c934f6d13ac0112b",
+    "url": "/SuppliesPro-Website/static/js/2.88b012f6.chunk.js"
+  },
+  {
+    "revision": "3f42c4fbfb720eda8be5",
+    "url": "/SuppliesPro-Website/static/js/main.0d077831.chunk.js"
+  },
+  {
+    "revision": "b4800c01da79845cf87a",
+    "url": "/SuppliesPro-Website/static/js/runtime~main.2468da4d.js"
+  }
+]);
